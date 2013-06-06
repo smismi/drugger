09f1307ac67bfd246409b98db7408b0ddb091a4e
smismi <smirnov.ru@gmail.com>
2013-06-06 23:04:54 +0400

    drag set to step

--- js/drugger.js
+++ js/drugger.js
@@ -16,8 +16,7 @@
             init: function (obj, options) {
 
                 var defaults = {
-                    a: null,
-                    b: null,
+                    values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                     step: null,
                     direction: 'horizontal'
                 }
@@ -57,53 +56,83 @@
 
 
                 this._steps = [];
+                this._values = [];
 
                 this.slider = {};
                 this.handler = {};
-
+                this.options.step = this.options.step - 1;
 
                 return this;
             },
+            reCalcSize: function () {
 
+
+
+                this.pos.lockd = false;
+
+                this.pos.x0 = event.pageX - $(this._handler).offset().left;
+                this.pos.y0 = event.pageY - $(this._handler).offset().top;
+
+                this.slider.width = $(this._slider).width();
+                this.slider.height = $(this._slider).height();
+
+                this.handler.width = $(this._handler).width();
+                this.handler.height = $(this._handler).height();
+
+                this.pos.xx = this.slider.width - $(this._handler).width();
+                this.pos.yy = this.slider.height - $(this._handler).height();
+//                this.options.step = this.options.step - 1;
+
+                _h = (this.options.direction === "horizontal") ? this.slider.width : this.slider.height;
+
+                _step_px = _h / this.options.step;
+
+                for (i = 0; i <= this.options.step; i++) {
+
+                    this._steps[i] = _step_px * i;
+                    this._values[i] = this.options.values[i];
+
+                }
+            },
             mouseon: function () {
-
-                _this = this;
 
 
                 this._handler.on({"mousedown": function (e) {
 
 
                     //получить коордитаты handles и мыши
+                    _this.reCalcSize();
 
 
-                    //abs el
-                    _this.pos.lockd = false;
-
-                    _this.pos.x0 = event.pageX - $(_this._handler).offset().left;
-                    _this.pos.y0 = event.pageY - $(_this._handler).offset().top;
-
-                    _this.slider.width = $(_this._slider).width();
-                    _this.slider.height = $(_this._slider).height();
-
-                    _this.handler.width = $(_this._handler).width();
-                    _this.handler.height = $(_this._handler).height();
-
-                    _this.pos.xx = _this.slider.width - $(_this._handler).width();
-                    _this.pos.yy = _this.slider.height - $(_this._handler).height();
-
-
-                    _h = (_this.options.direction === "horizontal") ? _this.slider.width : _this.slider.height;
-
-                    _step_px = _h / _this.options.step;
-
-                    for (i = 0; i <= _this.options.step; i++) {
-
-                        _this._steps[i] = _step_px * i;
-
-                    }
+//
+//                    //abs el
+//                    _this.pos.lockd = false;
+//
+//                    _this.pos.x0 = event.pageX - $(_this._handler).offset().left;
+//                    _this.pos.y0 = event.pageY - $(_this._handler).offset().top;
+//
+//                    _this.slider.width = $(_this._slider).width();
+//                    _this.slider.height = $(_this._slider).height();
+//
+//                    _this.handler.width = $(_this._handler).width();
+//                    _this.handler.height = $(_this._handler).height();
+//
+//                    _this.pos.xx = _this.slider.width - $(_this._handler).width();
+//                    _this.pos.yy = _this.slider.height - $(_this._handler).height();
+//
+//
+//                    _h = (_this.options.direction === "horizontal") ? _this.slider.width : _this.slider.height;
+//
+//                    _step_px = _h / _this.options.step;
+//
+//                    for (i = 0; i <= _this.options.step; i++) {
+//
+//                        _this._steps[i] = _step_px * i;
+//
+//                    }
 
 //                    debugger;
-                    $(document).on("mousemove", function () {
+                    $(document).off("mousemove").on("mousemove", function () {
 
                         if (_this.pos.lockd) return;
                         //новые координаты мыши
@@ -122,7 +151,7 @@
                         drag.call(_this, x, y);
                     });
 
-                    $(document).on("mouseup", function () {
+                    $(document).off("mouseup").on("mouseup", function () {
 
 
                         _this.pos.lockd = true;
@@ -153,14 +182,15 @@
                 return this;
             },
             drag: function() {
-                _this = this
 
-                _p = (_this.options.direction === "horizontal") ? x : y;
+                _this = this;
+
+                _p = (this.options.direction === "horizontal") ? x : y;
 
                 step = discret(_p, _step_px, this._steps);
 
-                y = (_this.options.direction === "vertical") ? (_step_px - (_this.handler.width / _this.options.step)) * step : 0;
-                x = (_this.options.direction === "horizontal") ? (_step_px - (_this.handler.height / _this.options.step)) * step : 0;
+                y = (this.options.direction === "vertical") ? (_step_px ) * step : 0;
+                x = (this.options.direction === "horizontal") ? (_step_px ) * step : 0;
 
 
                 this.setPosition(x, y)
@@ -190,27 +220,31 @@
             },
 
 
+            setToStep: function (step) {
+
+                this.reCalcSize();
+
+                step = (step > this.options.step) ? this.options.step + 1 : step;
+
+                y = (this.options.direction === "vertical") ? (_step_px - (this.handler.width / this.options.step)) * step : 0;
+                x = (this.options.direction === "horizontal") ? (_step_px - (this.handler.height / this.options.step)) * step : 0;
+
+                this.setPosition(x, y);
+            },
+            //TODO:
+            setToValue: function (value) {
+
+                this.reCalcSize();
+
+                step = (step > this.options.step) ? this.options.step : step;
+
+                y = (this.options.direction === "vertical") ? (_step_px - (this.handler.width / this.options.step)) * step : 0;
+                x = (this.options.direction === "horizontal") ? (_step_px - (this.handler.height / this.options.step)) * step : 0;
+
+                this.setPosition(x, y);
+            },
 
 
-                                    event: function () {
-
-                                        var setToStep = function (step) {
-
-                                            step = (step > _this.options.step) ? _this.options.step : step;
-
-                                            y = (_this.options.direction === "vertical") ? (_step_px - (_this.handler.width / _this.options.step)) * step : 0;
-                                            x = (_this.options.direction === "horizontal") ? (_step_px - (_this.handler.height / _this.options.step)) * step : 0;
-
-                                            setPosition(x, y);
-                                        };
-
-                                        var setToValue = function (value) {
-
-
-                                            setPosition(x, y);
-                                        };
-
-                                    },
 
 
 
@@ -250,7 +284,7 @@
 //		_handler.on(_e.eventClick , function(e){
 //			console.log(e)
 //		})
-
+        return drugger;
 
     }
 
